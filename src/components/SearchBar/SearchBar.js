289eb9dc05c46941522fde06ce3f0ea747149b32
lukaakhalbedashvili <lukaakhalbedashvili@gmail.com>
2022-03-03 01:16:38 +0400

    update input Component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,11 +1,40 @@
+import React, { useState } from "react"
 import Styles from "./SearchBar.module.scss"
+import { CircularProgress } from "@mui/material"
 import { IoIosSearch } from "react-icons/io"
-
+import classnames from "classnames"
 const SearchBar = () => {
+  const [input, setInput] = useState("")
   return (
-    <div className={Styles.SearchBarDiv}>
-      <IoIosSearch className={Styles.SearchIcon} size={35} />
-      <input className={Styles.SearchBarInput} />
+    <div className={Styles.MainSearchDiv}>
+      <div className={Styles.SearchBarDiv}>
+        {input.length > 0 && (
+          <p className={Styles.SearchIcon}>
+            <CircularProgress color="inherit" size={18} />
+          </p>
+        )}
+        {input.length < 1 && (
+          <IoIosSearch className={Styles.SearchIcon} size={35} />
+        )}
+        <input
+          className={classnames({
+            [Styles.SearchBarInputFull]: input.length > 0,
+            [Styles.SearchBarInput]: input.length < 1,
+          })}
+          spellCheck={false}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        {input.length > 0 && (
+          <p className={Styles.SearchBarClearLabel}>Clear</p>
+        )}
+      </div>
+
+      <div
+        className={classnames({
+          [Styles.SearchResults]: input.length < 1,
+          [Styles.SearchResultsFull]: input.length > 0,
+        })}
+      ></div>
     </div>
   )
 }
