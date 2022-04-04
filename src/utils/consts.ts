9648ef152f1e8e718b244c739e028237803fb9e8
lukaakhalbedashvili <lukaakhalbedashvili@gmail.com>
2022-04-04 17:04:40 +0400

    general stage

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,5 +1,5 @@
-export const registerCompanyFrameImage = "/images/signup.JPG"
-export const generalCompanyFrameImage = "/images/generalInfo.JPG"
+export const registerCompanyFrameImage = "/signup.JPG"
+export const generalCompanyFrameImage = "/generalInfo.JPG"
 export const singUpIputsArray = [
   { label: "First Name", name: "firstName", type: "text", value: "" },
   { label: "Password", name: "password", type: "password", value: "" },
